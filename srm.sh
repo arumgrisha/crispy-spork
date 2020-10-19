a9a2f7e9ae573808d41184937faf0f0170d9680c
arumgrisha <joblavore@gmail.com>
2020-10-19 18:07:57 +0000

    Фича | удаление файлов из корзины старше 7 дней

--- srm.sh
+++ srm.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 #
-rec_dir=~/RECYCLE/
-if test -z $1;
+rec_dir=~/RECYCLE/ #Определяем местоположение корзины
+if test -z $1; #Проверяем получение аргумента (названия файла)
 	then
-		echo "There is no filename in argument"
+		echo "There is no filename in argument";
+		return #Выходим из функции
 	else
 		echo "Safe removing: $1"
 fi
-echo $rec_dir
 
-if test -d $rec_dir;
+#echo $rec_dir
+
+if test -d $rec_dir; #Проверяем существование корзины
 	then
 		echo "~/RECYCLE exists"
 	else
 		echo "Creating ~/RECYCLE";
 		mkdir $rec_dir
+		return #Выходим из функции
 fi
 
-#это комментарий многостроковый
+#Это комментарий многостроковый
 #: '
-#smth
+#Команды
 #'
 
 #if [[ $1 = "" ]];
@@ -29,16 +32,16 @@ fi
 #		echo "$1"
 #fi
 
-rm_file_string=$1 # вводить можно только название файла в которой находитесь. В названии не должно быть пути. В директории RECYCLE вводить нельзя.
+rm_file_string=$1 # Вводить можно только название файла. Находится нужно в этой же директории, где лежит файл. В названии не должно быть пути. В директории RECYCLE вводить нельзя.
 touch ./$rm_file_string #для отладки, потом удалить
 mv $rm_file_string $rec_dir
-echo $rm_file_string
+#echo $rm_file_string
 
 temp1="$rec_dir""$rm_file_string"
 gzip $temp1
 temp1=""
 
-echo "Succeed in srm $rm_file_string"
+#echo "Succeed in srm $rm_file_string"
 
 #rm_file_string_2="$rm_file_string"".gz" #для отладки, потом удалить
 #rm_file_string_2=${rm_file_string + ".gz"} #для отладки, потом удалить. Или так добавить окончание .gz. Чего-то не работает
@@ -50,7 +53,10 @@ temp1=""
 temp1="$rec_dir""temp_folder"
 echo $temp1
 touch $temp1
-find -mtime +6 ${rec_dir::-1} #| >temp1
-
-#rm $temp1
+find $rec_dir -mtime +6 -fprint $temp1
+while read line;
+	do
+		rm $line;
+	done < $temp1
+rm $temp1
 temp1=""
